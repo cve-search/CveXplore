36f4416ab978a4d90984581f8a3652bcded52b74
Paul Tikken Laptop <paul.tikken@gmail.com>
2021-06-29 17:43:02 +0000

    minor

--- CveXplore/cli_cmds/db_cmds/commands.py
+++ CveXplore/cli_cmds/db_cmds/commands.py
@@ -10,10 +10,12 @@ def db_cmd(ctx):
 
 
 @db_cmd.group("update", invoke_without_command=True, help="Update the database")
+@click.pass_context
 def update_cmd(ctx):
     ctx.obj["data_source"].database.update()
 
 
 @db_cmd.group("populate", invoke_without_command=True, help="Populate the database")
+@click.pass_context
 def populate_cmd(ctx):
     ctx.obj["data_source"].database.populate()
