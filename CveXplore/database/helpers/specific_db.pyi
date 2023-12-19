73c58897248d85de448a83bddeb70b2d96528de2
Paul Tikken <paul.tikken@gmail.com>
2023-12-19 15:14:18 +0000

    [CveXplore-231] added stub file for dynamically generated attributes in collection classes

---
+++ CveXplore/database/helpers/specific_db.pyi
@@ -0,0 +1,45 @@
+from CveXplore.database.helpers.generic_db import GenericDatabaseFieldsFunctions
+
+class CvesDatabaseFunctions:
+    id: GenericDatabaseFieldsFunctions
+    cvss: GenericDatabaseFieldsFunctions
+    cvss3: GenericDatabaseFieldsFunctions
+    summary: GenericDatabaseFieldsFunctions
+    vendors: GenericDatabaseFieldsFunctions
+    products: GenericDatabaseFieldsFunctions
+    lastModified: GenericDatabaseFieldsFunctions
+    modified: GenericDatabaseFieldsFunctions
+    published: GenericDatabaseFieldsFunctions
+    status: GenericDatabaseFieldsFunctions
+    assigner: GenericDatabaseFieldsFunctions
+    cwe: GenericDatabaseFieldsFunctions
+    epss: GenericDatabaseFieldsFunctions
+
+    def __init__(self, collection: str): ...
+
+class CpeDatabaseFunctions:
+    id: GenericDatabaseFieldsFunctions
+    title: GenericDatabaseFieldsFunctions
+    cpeName: GenericDatabaseFieldsFunctions
+    vendor: GenericDatabaseFieldsFunctions
+    product: GenericDatabaseFieldsFunctions
+    stem: GenericDatabaseFieldsFunctions
+
+    def __init__(self, collection: str): ...
+
+class CapecDatabaseFunctions:
+    name: GenericDatabaseFieldsFunctions
+    summary: GenericDatabaseFieldsFunctions
+    prerequisites: GenericDatabaseFieldsFunctions
+    solutions: GenericDatabaseFieldsFunctions
+    loa: GenericDatabaseFieldsFunctions
+    typical_severity: GenericDatabaseFieldsFunctions
+
+    def __init__(self, collection: str): ...
+
+class CWEDatabaseFunctions:
+    name: GenericDatabaseFieldsFunctions
+    status: GenericDatabaseFieldsFunctions
+    description: GenericDatabaseFieldsFunctions
+
+    def __init__(self, collection: str): ...
