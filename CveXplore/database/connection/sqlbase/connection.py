9d57adc15b207d7813ec2db2708693f34f3f3100
Paul Tikken <paul.tikken@gmail.com>
2023-12-20 16:27:24 +0000

    [CveXplore-241] wip

---
+++ CveXplore/database/connection/sqlbase/connection.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from CveXplore.common.config import Configuration
+
+config = Configuration()
+
+engine = create_engine(config.SQLALCHEMY_DATABASE_URI, echo=True)
+
+Session = sessionmaker(bind=engine)
